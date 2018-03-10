698ee56465026e61067d0eec1f132e1f84e9bec3
Joel Helbling <joel@joelhelbling.com>
2018-03-09 23:39:29 -0500

    adds source worker to DSL

--- lib/stepladder/dsl.rb
+++ lib/stepladder/dsl.rb
@@ -2,14 +2,32 @@ module Stepladder
   class WorkerInitializationError < StandardError; end
 
   module Dsl
+    def source(argument=nil, &block)
+      ensure_correct_arity_for!(argument, block)
+
+      series = series_from(argument)
+      callable = setup_callable_for(block, series)
+
+      return Worker.new(&callable) if series.nil?
+
+      Worker.new do
+        series.each(&callable)
+
+        while true do
+          handoff nil
+        end
+      end
+
+    end
+
     def filter(argument=nil, &block)
       if (block && argument.respond_to?(:call))
-        raise WorkerInitializationError.new("You cannot supply two callables")
+        throw_with 'You cannot supply two callables'
       end
       callable = argument.respond_to?(:call) ? argument : block
 
       unless callable
-        raise WorkerInitializationError.new("You must supply a callable")
+        throw_with 'You must supply a callable'
       end
       Worker.new filter: block
     end
@@ -17,5 +35,49 @@ module Stepladder
     def handoff(something)
       Fiber.yield something
     end
+
+    private
+
+    def throw_with(*msg)
+      raise WorkerInitializationError.new([msg].flatten.join(' '))
+    end
+
+    def ensure_correct_arity_for!(argument, block)
+      return unless block
+      if argument
+        if block.arity != 1
+          throw_with 'Source worker with enumerable argument',
+                     'and block must accept exactly one argument',
+                     '(arity == 1)'
+        end
+      else
+        if block.arity > 0
+          throw_with \
+            'Source worker cannot accept any arguments (arity == 0)'
+        end
+      end
+    end
+
+    def series_from(series)
+      return if series.nil?
+      case
+      when series.respond_to?(:to_a)
+        series.to_a
+      when series.respond_to?(:scan)
+        series.scan(/./)
+      else
+        [series]
+      end
+    end
+
+    def setup_callable_for(block, series)
+      return block unless series
+      if block
+        return Proc.new { |value| handoff block.call(value) }
+      else
+        return Proc.new { |value| handoff value }
+      end
+    end
+
   end
 end
--- spec/lib/stepladder/dsl_spec.rb
+++ spec/lib/stepladder/dsl_spec.rb
@@ -6,50 +6,148 @@ module Stepladder
   describe Dsl do
     include Stepladder::Dsl
 
-    Given(:worker) { Worker.new } #placeholder
-    Given(:source) do
-      Worker.new do
-        numbers = (1..3).to_a
-        until numbers.empty?
-          handoff numbers.shift
+    describe '#source' do
+      context 'normal usage' do
+        context 'with an array' do
+          Given(:worker) { source [:fee, :fi] }
+
+          Then { worker.product == :fee }
+          And  { worker.product == :fi }
+          And  { worker.product.nil? }
+        end
+
+        context 'with a range' do
+          Given(:worker) { source (0..2) }
+
+          Then { worker.product == 0 }
+          And  { worker.product == 1 }
+          And  { worker.product == 2 }
+          And  { worker.product.nil? }
+        end
+
+        context 'with a string' do
+          Given(:worker) { source 'abc' }
+
+          Then { worker.product == 'a' }
+          And  { worker.product == 'b' }
+          And  { worker.product == 'c' }
+          And  { worker.product.nil? }
+        end
+
+        context 'with a hash' do
+          Given(:worker) { source({foo: 2, bar: 'yarr'}) }
+
+          Then { worker.product == [:foo, 2] }
+          And  { worker.product == [:bar, 'yarr'] }
+          And  { worker.product.nil? }
+        end
+
+        context 'with a anything else' do
+          Given(:worker) { source :foo }
+
+          Then { worker.product == :foo }
+          And  { worker.product.nil? }
+        end
+
+        context 'with a callable' do
+          Given(:worker) do
+            notes = %i[doh ray me fa so la ti]
+            source do
+              notes.shift if notes.size > 4
+            end
+          end
+
+          Then { worker.product == :doh }
+          And  { worker.product == :ray }
+          And  { worker.product == :me }
+          And  { worker.product.nil? }
+        end
+
+        context 'with a callable and an argument' do
+          Given(:notes) { %i[so la ti] }
+          Given(:worker) do
+            source notes do |note|
+              note.to_s.upcase
+            end
+          end
+
+          Then { worker.product == 'SO' }
+          And  { worker.product == 'LA' }
+          And  { worker.product == 'TI' }
+          And  { worker.product.nil? }
+        end
+      end
+
+      context 'illegal usage' do
+        context 'with no argument and arity > 0' do
+          Given(:invocation) do
+            -> { source { |v| v * 2 } }
+          end
+          Then { expect(invocation).to raise_error(/arity == 0/) }
+        end
+      end
+
+      context 'with argument' do
+        context 'and arity == 0' do
+          Given(:invocation) do
+            -> { source([]) { :boo } }
+          end
+          Then { expect(invocation).to raise_error(/arity == 1/) }
+        end
+        context 'and arity > 1' do
+          Given(:invocation) do
+            -> { source([]) { |p, q| :boo } }
+          end
+          Then { expect(invocation).to raise_error(/arity == 1/) }
         end
       end
     end
 
-    When { source | worker }
-
     describe '#filter' do
-      context 'in normal usage' do
-        Given(:worker) do
+      context 'normal usage' do
+        Given(:source) do
+          Worker.new do
+            numbers = (1..3).to_a
+            until numbers.empty?
+              handoff numbers.shift
+            end
+          end
+        end
+
+        When { source | filter_worker }
+
+        Given(:filter_worker) do
            filter { |v| v % 2 == 0 }
         end
 
-        Then { worker.product == 2 }
-        And { expect(worker.product).to be_nil }
-      end
-    end
-
-    context 'requires a callable' do
-      context 'with no arguments or block' do
-        Given(:invocation) { -> { filter } }
-        Then { expect(invocation).to raise_error(/supply a callable/) }
+        Then { filter_worker.product == 2 }
+        And { expect(filter_worker.product).to be_nil }
       end
 
-      context 'with no callable' do
-        Given(:invocation) { -> { filter :foo } }
-        Then { expect(invocation).to raise_error(/supply a callable/) }
-      end
+      context 'illegal usage' do
+        context 'requires a callable' do
+          context 'with no arguments or block' do
+            Given(:invocation) { -> { filter } }
+            Then { expect(invocation).to raise_error(/supply a callable/) }
+          end
 
-      context 'with callable arg' do
-        Given(:arg) { Proc.new { true } }
-        Given(:invocation) { -> { filter arg } }
-        Then { expect(invocation).to_not raise_error }
-      end
+          context 'with no callable' do
+            Given(:invocation) { -> { filter :foo } }
+            Then { expect(invocation).to raise_error(/supply a callable/) }
+          end
 
-      context 'with both callable arg and block' do
-        Given(:arg) { Proc.new { true } }
-        Given(:invocation) { -> { filter(arg) do false; end } }
-        Then { expect(invocation).to raise_error(/two callables/) }
+          context 'with callable arg' do
+            Given(:arg) { Proc.new { true } }
+            Given(:invocation) { -> { filter arg } }
+            Then { expect(invocation).to_not raise_error }
+          end
+
+          context 'with both callable arg and block' do
+            Given(:arg) { Proc.new { true } }
+            Given(:invocation) { -> { filter(arg) do false; end } }
+            Then { expect(invocation).to raise_error(/two callables/) }
+          end
+        end
       end
     end
   end
